6d8c2fca71358562256dc240501b9d31ef19c5a2
Georgy Zhukov <g.zhukov@ripe.capital>
2024-09-20 10:44:39 +0300

    Refactoring

--- src/utils.py
+++ src/utils.py
@@ -1,27 +1,22 @@
 import json
-import os
 from pathlib import Path
 
 import pandas as pd
 import requests
 from prefect import task
-from dotenv import load_dotenv
+
+from config import TELEGRAM_CHAT_ID, TELEGRAM_TOKEN
 
 
-load_dotenv()
-
-TELEGRAM_TOKEN: str = os.getenv("TELEGRAM_TOKEN")
-TELEGRAM_CHAT_ID: str = os.getenv("TELEGRAM_CHAT_ID")
-
-
-def load_csv(file_path: str) -> pd.DataFrame:
+@task  # cache_policy
+def load_csv(file_path: str, sep: str) -> pd.DataFrame:
     try:
-        return pd.read_csv(file_path, sep=",")
+        return pd.read_csv(file_path, sep=sep)
     except Exception as e:
         raise RuntimeError(f"Ошибка при загрузке CSV файла: {e}")
 
 
-@task(retries=3, retry_delay_seconds=5)
+@task(retries=3, retry_delay_seconds=5)  # cache_policy
 def fetch_data_from_api(row: pd.Series) -> dict:
     api_url = "https://www.alphavantage.co/query"
     params = {
@@ -44,9 +39,6 @@ def process_api_data(data: dict) -> tuple[pd.DataFrame, dict]:
     time_series: dict = data.get("Time Series (Daily)", {})
 
     df: pd.DataFrame = pd.DataFrame.from_dict(time_series, orient='index')
-    df.index = pd.to_datetime(df.index)
-    df.reset_index(inplace=True)
-    df.rename(columns={'index': 'date'}, inplace=True)
 
     df = df.rename(columns={
         "1. open": "open",
@@ -63,7 +55,7 @@ def process_api_data(data: dict) -> tuple[pd.DataFrame, dict]:
 @task
 def save_data_to_json(data: pd.DataFrame, meta_data: dict, output_dir: str, file_name: str) -> None:
     output_path = Path(output_dir) / f"{file_name}.json"
-    data_dict: dict = data.to_dict(orient="records")
+    data_dict = data.to_dict(orient="records")
 
     combined_data: dict = {
         "meta_data": meta_data,
